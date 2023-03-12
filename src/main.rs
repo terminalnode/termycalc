0dfccb84b45a96c089929b1c08731d667e2919c4
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2023-03-12 19:40:05 +0100

    make window same size as grid

--- src/main.rs
+++ src/main.rs
@@ -73,7 +73,7 @@ fn build_ui(
         .child(&grid)
         .build();
 
-    window.set_default_size(400, 400);
+    window.set_default_size(grid.width(), grid.height());
     window.present();
 }
 
